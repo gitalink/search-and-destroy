5f3cf9f5db14c7beb9445b35ac2bcc0d4c4a0664
Liz Gordon <lizgordon14@gmail.com>
2019-12-11 16:17:02 -0500

    binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,23 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if(array.length === 1) {
+		if(target === array[0]){
+			return true
+		} else {
+			return false
+		}
+	}
+	const first = array.slice(0, Math.floor(array.length/2))
+	const second = array.slice(Math.floor(array.length/2))
+
+
+
+	if(second[0]>target){
+		return binarySearch(first, target)
+	}else{
+		return binarySearch(second, target)
+	}
 };
 
 /*
@@ -13,5 +29,7 @@ const binarySearch = (array, target) => {
 	How can we use other pieces of data, like pointers, to avoid slicing?
 
 */
+
+
 
 module.exports = binarySearch
